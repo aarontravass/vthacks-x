be7741650441425a46bc3da7931c14e311c16050
aarontravass <aaron.travass@hotmail.com>
2022-11-12 16:29:51 -0500

    added heroku yml

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { json as bodyparserJSON, urlencoded as bodyparserURLEncoded } from 'body-parser';
+import {
+    json as bodyparserJSON,
+    urlencoded as bodyparserURLEncoded
+} from 'body-parser';
 import { json as expressJSON, urlencoded as expressURLEncoded } from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
-    const CORS_URLS = ['localhost']
+    const CORS_URLS = ['localhost'];
     app.enableCors({
         origin: CORS_URLS,
         methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH'],
@@ -23,7 +26,6 @@ async function bootstrap() {
         optionsSuccessStatus: 200
     });
 
-
     app.use(bodyparserJSON({ limit: '10mb' }));
     app.use(
         bodyparserURLEncoded({
@@ -33,7 +35,13 @@ async function bootstrap() {
         })
     );
     app.use(expressJSON({ limit: '3mb' }));
-    app.use(expressURLEncoded({ limit: '10mb', extended: true, parameterLimit: 100 }));
+    app.use(
+        expressURLEncoded({
+            limit: '10mb',
+            extended: true,
+            parameterLimit: 100
+        })
+    );
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
