3f019c8c99d45c010be5bd0134f6214dccf6c9a3
aarontravass <aaron.travass@hotmail.com>
2022-11-12 15:49:56 -0500

    exposed port from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,14 @@ import { CarbonModule } from './carbon_emission/carbon.module';
 import { DatabaseModule } from './core/database/database.module';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true
-        }),
-        DatabaseModule,
-        CarbonModule
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    CarbonModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
